c5f719225b26ddb83cd5c3d4787bbaba0e88914f
Caleb Tutty <caleb.tutty@gmail.com>
2018-11-26 21:33:09 +1300

    Use shell_out! over deprecated and removed run_command_with_systems_locale

--- libraries/gdebi_provider.rb
+++ libraries/gdebi_provider.rb
@@ -6,12 +6,12 @@ class Chef
       class Gdebi < Chef::Provider::Package::Dpkg
         def install_package(name, version)
           opts = @new_resource.options ? "--option='#{@new_resource.options}'" : ""
-          run_command_with_systems_locale(
-                                          :command => "gdebi --quiet --non-interactive #{opts} #{@new_resource.source}",
-                                          :environment => {
-                                            "DEBIAN_FRONTEND" => "noninteractive"
-                                          }
-                                          )
+          shell_out!(
+            :command => "gdebi --quiet --non-interactive #{opts} #{@new_resource.source}",
+            :environment => {
+                "DEBIAN_FRONTEND" => "noninteractive"
+              }
+            )
         end
       end
     end
